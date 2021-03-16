b69bf4aa1f24f030d34935c1a9f28c82fea7dc34
Cotton Malone <ctnmalone@gmail.com>
2021-03-16 17:18:00 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,93 @@
+Line processing utility using Javascript as a proof of concept
+
+Requires [QuickJS](https://github.com/ctn-malone/quickjs-cross-compiler/releases/tag/2020-11-08_3%2Bext-lib-0.1.0) for compilation
+
+# Compile
+
+```
+cd src
+qjsc.sh -o qjslp qjslp.js
+```
+
+# Usage
+
+```
+qjslp -h
+Line processing utility using Javascript (0.1.0)
+Usage: qjslp [-h|--help] [-c,--code] [-b,--begin] [-e|--end] [-q|--quiet] [-d|--debug] [--debug-store]
+    -c  --code:     code to execute for each line ($_ is the current line, $$ is the global store object,
+                    $i is the 0-based index of current line)
+                    Can be specified multiple times
+    -b, --begin:    code to execute before processing lines ($$ is the global store object)
+    -e, --end:      code to execute after all lines have been processed ($$ is the global store object,
+                    $c is the number of lines which were processed)
+    -q, --quiet:    if used, input lines will never be printed
+    -d, --debug:    print debug information during processing (implies -q)
+    --debug-store:  print store whenever a store value has changed (ignored if -d is not set)
+    -h, --help:     print help
+```
+
+# Examples
+
+<u>NB</u> : a better alternative is likely to exist using common unix tools ;)
+
+* print the number of entries for each state in file `01.csv`, in JSON format
+  * initialize `counter` object in store
+  * for each line
+    * ignore if line is first one (header)
+    * store current state in store
+    * initialize counter for current state if needed
+    * increase counter for current state
+  * print `counter` object
+
+```
+cat 01.csv | qjslp -q \
+    -b '$$.count = {}' \
+    -c 'return $i > 0' \
+    -c '$$.state = $_.split(",")[9].trim()' \
+    -c '$$.count[$$.state] ||= 0' \
+    -c '++$$.count[$$.state]' \
+    -e '$p(JSON.stringify($$.count))'
+```
+
+* convert `01.csv` to JSON format
+  * initialize `entries` array in store
+  * for each line
+    * remove `"`
+    * if line is first one (header), split line and store result in store
+    * split line, build a JS object and add it to the `entries` array
+  * print `entries` object
+
+```
+cat 01.csv | qjslp -q \
+    -b '$$.entries = []' \
+    -c '$_!.replace(/"/g, "")' \
+    -c 'if ($i == 0) { $$.fields = $_.split(",").map(e => e.trim()) ; return false }' \
+    -c 'entry = {} ; $_.split(",").forEach((e, i) => entry[$$.fields[i]] = e.trim()) ; $$.entries.push(entry)' \
+    -e '$p(JSON.stringify($$.entries))'
+```
+
+* extract `server` section from `02.ini`
+  * enable `ignore` flag in store
+  * for each line
+    * if `[server]` section is starting, disable `ignore` flag
+    * if another section is starting, enable `ignore` flag
+
+```
+cat 02.ini | qjslp \
+    -b '$$.ignore = true' \
+    -c 'if ($_.startsWith("[")) { $$.ignore = ($_ != "[server]") } ; return !$$.ignore'
+```
+
+* split lines in `03.csv` based on sex
+  * for each line
+    * ignore if line is first one
+    * if second field contains `F`, write line to *stdout*
+    * otherwise write line to *stderr*
+
+```
+cat 03.csv | qjslp -q \
+    -c 'return $i > 0' \
+    -c '$_.split(",")[1].trim() == "\"F\"" ? $p($_) : $e($_)' \
+    >/tmp/F.csv 2>/tmp/M.csv
+```
---
+++ src/qjslp.js
@@ -0,0 +1,307 @@
+import * as std from 'std';
+import * as os from 'os';
+import arg from 'ext/arg.js';
+import { getScriptName } from 'ext/path.js';
+
+/*
+    Simple line processor to be used in pipelines (proof of concept)
+
+    In line processing code
+    
+        - $_ is current line
+        - $$ is global store
+        - $i is the 0-based index of current line
+      
+        If function returns {undefined}, {null} or {false} line will be ignored
+        If function return something which is not a string or a number, input line will keep its value
+        If {$_} is assigned a new value, this new value will be printed
+
+        When using $_!.{fn}, code will be replaced with $_ = $_.{fn}
+        
+        Example
+
+            $_!.toUpperCase() => $_ = $_.toUpperCase()
+        
+        When using multiple -c arguments, they will be processed in the order the were defined
+
+        If no -c argument is given, input line will be printed (unless -q was used)
+
+    In begin & end code
+
+        - $$ is global store
+    
+    In end code
+
+        - $c is the number of lines which were processed
+    
+    In all functions
+
+        - $p is an helper function which prints to stdout (\n is added)
+        - $e is an helper function which prints to stderr (\n is added)
+ */
+
+const VERSION = '0.1.0';
+
+const getUsage = () => {
+    const message = `
+Usage: ${getScriptName()} [-h|--help] [-c,--code] [-b,--begin] [-e|--end] [-q|--quiet] [-d|--debug] [--debug-store]
+    -c  --code:     code to execute for each line ($_ is the current line, $$ is the global store object,
+                    $i is the 0-based index of current line)
+                    Can be specified multiple times
+    -b, --begin:    code to execute before processing lines ($$ is the global store object)
+    -e, --end:      code to execute after all lines have been processed ($$ is the global store object,
+                    $c is the number of lines which were processed)
+    -q, --quiet:    if used, input lines will never be printed
+    -d, --debug:    print debug information during processing (implies -q)
+    --debug-store:  print store whenever a store value has changed (ignored if -d is not set)
+    -h, --help:     print help
+`.trim();
+    return message;
+}
+
+const getHelp = () => {
+    const message = `
+Line processing utility using Javascript (${VERSION})
+`.trim();
+    return `${message}\n${getUsage()}`;
+}
+
+// list of functions used to process lines;
+globalThis._lineProcessingFn = [];
+globalThis.$p = (...args) => { std.out.puts(`${args.join(' ')}\n`) };
+globalThis.$e = (...args) => { std.err.puts(`${args.join(' ')}\n`) };
+
+let args;
+try {
+    args = arg({
+        '--begin': (v, n, p) => {
+            const value = v.trim();
+            if ('' != value) {
+                const beginFn = `function _begin($$) { ${value} }`;
+                try {
+                    std.evalScript(beginFn);
+                }
+                catch (e) {
+                    const err = new Error(`Begin code cannot not be evaluated: ${e.message.trim()}`);
+                    err.code = 'ARG_INVALID_OPTION';
+                    throw err;
+                }
+            }
+            return value;
+        },
+        '--end': (v, n, p) => {
+            const value = v.trim();
+            if ('' != value) {
+                const endFn = `function _end($$, $c) { ${value} }`;
+                try {
+                    std.evalScript(endFn);
+                }
+                catch (e) {
+                    const err = new Error(`End code cannot not be evaluated: ${e.message.trim()}`);
+                    err.code = 'ARG_INVALID_OPTION';
+                    throw err;
+                }
+            }
+            return value;
+        },
+        '--code': (v, n, p) => {
+            let value = v.trim();
+            value = value.replace(/\$_!/g, '$_ = $_');
+            const index = globalThis._lineProcessingFn.length;
+            if ('' != value) {
+                globalThis._lineProcessingFn.push({str:value});
+                const str = `globalThis._lineProcessingFn[${index}].fn = function ($_, $$, $i) { ${value} ; return $_ }`;
+                try {
+                    std.evalScript(str);
+                }
+                catch (e) {
+                    const err = new Error(`Line processing code #${index + 1} (${value}) cannot not be evaluated: ${e.message.trim()}`);
+                    err.code = 'ARG_INVALID_OPTION';
+                    throw err;
+                }
+            }
+            return value;
+        },
+        '--help': Boolean,
+        '--quiet': Boolean,
+        '--debug': Boolean,
+        '--debug-store': Boolean,
+        // aliases
+        '-h': '--help',
+        '-c': '--code',
+        '-b': '--begin',
+        '-e': '--end',
+        '-q': '--quiet',
+        '-d': '--debug'
+    });
+}
+catch (e) {
+    switch (e.code) {
+        case 'ARG_UNKNOWN_OPTION':
+        case 'ARG_INVALID_OPTION':            
+        case 'ARG_MISSING_REQUIRED_SHORTARG':
+        case 'ARG_MISSING_REQUIRED_LONGARG':
+            std.err.printf(`${e.message.trim()}\n`);
+            std.err.printf(`${getUsage()}\n`);
+            std.exit(2);
+    }
+    throw e;
+}
+if (args['--help']) {
+    std.err.printf(`${getHelp()}\n`);
+    std.exit(2);
+}
+if (args['--debug']) {
+    args['--quiet'] = true;
+}
+else if (args['--debug-store']) {
+    args['--debug-store'] = false;
+}
+
+// store available in begin code, line processing code & end code
+const store = {};
+let prevStore;
+
+// execute begin code
+if ('function' == typeof _begin) {
+    if (args['--debug']) {
+        std.err.printf(`[begin]\n`);
+        std.err.flush();
+    }
+    try {
+        _begin(store);
+    }
+    catch (e) {
+        std.err.printf(`Begin code failed: ${e.message.trim()}\n`);
+        std.exit(1);
+    }
+    if (args['--debug']) {
+        std.err.printf(`[/begin]\n`);
+        std.err.printf(`${JSON.stringify(store, null, 2)}\n`);
+        std.err.flush();
+    }
+}
+
+let inLine;
+let outLine;
+let lineNumber = 1;
+let tmpOutline;
+let ignoreLine = false;
+let shouldBreak = false;
+while (null !== (inLine = std.in.getline())) {
+    outLine = inLine;
+    ignoreLine = false;
+    shouldBreak = false;
+    if (args['--debug']) {
+        std.err.printf(`[line #${lineNumber}]\n`);
+        std.err.printf(`  in: ${JSON.stringify(inLine)}\n`);
+        std.err.flush();
+    }
+    // call each line processing function
+    for (let i = 0; i < globalThis._lineProcessingFn.length; ++i) {
+        if (args['--debug']) {
+            std.err.printf(`  [func #${i + 1}]\n`);
+            std.err.printf(`    in:  ${JSON.stringify(outLine)}\n`);
+            std.err.flush();
+            if (args['--debug-store']) {
+                prevStore = JSON.stringify(store);
+            }
+        }
+        try {
+            tmpOutline = globalThis._lineProcessingFn[i].fn(outLine, store, lineNumber - 1);
+        }
+        catch (e) {
+            std.err.printf(`Line processing code #${i + 1} (${globalThis._lineProcessingFn[i].str}) failed when processing line #${lineNumber} (line will be printed below): ${e.message.trim()}\n`);
+            std.err.printf(`  ${JSON.stringify(outLine)}\n`);
+            std.err.printf(`Initial line will be printed below\n`);
+            std.err.printf(`  ${JSON.stringify(inLine)}\n`);
+            std.exit(1);
+        }
+        // we didn't receive a string or a number
+        if ('string' != typeof tmpOutline && 'number' != typeof tmpOutline) {
+            // filter line if result is {undefined}, {null} or {false}
+            if (undefined == tmpOutline || null === tmpOutline || false === tmpOutline) {
+                outLine = tmpOutline;
+                ignoreLine = true;
+                if (args['--debug']) {
+                    std.err.printf(`    out: IGNORED\n`);
+                    std.err.printf(`  [/func #${i + 1}]\n`);
+                    std.err.flush();
+                }
+                shouldBreak = true;
+            }
+            else {
+                if (args['--debug']) {
+                    std.err.printf(`    out: ${JSON.stringify(outLine)}\n`);
+                    std.err.printf(`  [/func #${i + 1}]\n`);
+                    std.err.flush();
+                }
+            }
+        }
+        else {
+            // update outLine for next line processing function
+            outLine = tmpOutline;
+            if (args['--debug']) {
+                std.err.printf(`    out: ${JSON.stringify(outLine)}\n`);
+                std.err.printf(`  [/func #${i + 1}]\n`);
+                std.err.flush();
+            }
+        }
+        if (args['--debug-store']) {
+            const newStore = JSON.stringify(store);
+            if (newStore != prevStore) {
+                std.err.printf(`${JSON.stringify(store, null, 2)}\n`);
+                std.err.flush();
+            }
+        }
+        if (shouldBreak) {
+            break;
+        }
+    }
+    if (!ignoreLine) {
+        if (!args['--quiet']) {
+            // if true, output what we received on stdin
+            if (true === outLine) {
+                std.out.puts(`${inLine}\n`);
+            }
+            // output what was returned by function
+            else {
+                std.out.puts(`${outLine}\n`);
+            }
+            std.out.flush();
+        }
+        if (args['--debug']) {
+            std.err.printf(`  out: ${JSON.stringify(outLine)}\n`);
+            std.err.printf(`[/line #${lineNumber}]\n`);
+            std.err.flush();
+        }
+    }
+    else {
+        if (args['--debug']) {
+            std.err.printf(`  out: IGNORED\n`);
+            std.err.printf(`[/line #${lineNumber}]\n`);
+            std.err.flush();
+        }
+    }
+    ++lineNumber;
+}
+
+// execute end code
+if ('function' == typeof _end) {
+    if (args['--debug']) {
+        std.err.printf(`[end]\n`);
+        std.err.flush();
+    }
+    try {
+        _end(store, lineNumber - 1);
+    }
+    catch (e) {
+        std.err.printf(`End code failed: ${e.message.trim()}\n`);
+        std.exit(1);
+    }
+    if (args['--debug']) {
+        std.err.printf(`[/end]\n`);
+        std.err.printf(`${JSON.stringify(store, null, 2)}\n`);
+        std.err.flush();
+    }
+}
